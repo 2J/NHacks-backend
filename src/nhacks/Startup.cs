101c105699e93156f3287e7c6a9ab956542f2eb2
Justin Jeong <jj@jj.ai>
2016-03-12 11:48:18 -0500

    Account API functionality

--- src/nhacks/Startup.cs
+++ src/nhacks/Startup.cs
@@ -44,7 +44,14 @@ namespace nhacks
                 .AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(o => {
+                // configure identity options
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonLetterOrDigit = false; ;
+                o.Password.RequiredLength = 6;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
